f885198ac2c9671709fdc00a5eb678c5597069cc
Hugo AVRIL <hugoavril2910@gmail.com>
2023-04-04 16:15:39 +0200

    fix target

--- src/main/java/com/genericite/TableauAlg.java
+++ src/main/java/com/genericite/TableauAlg.java
@@ -7,9 +7,9 @@ public class TableauAlg {
         String max = chaines[0];
 
         for (String chaine : chaines) {
-            if (min.compareTo(chaine) > 0)
+            if (min.compareTo(chaine) < 0)
                 min = chaine;
-            if (max.compareTo(chaine) < 0)
+            if (max.compareTo(chaine) > 0)
                 max = chaine;
         }
         return new Paire<String>(min, max);
